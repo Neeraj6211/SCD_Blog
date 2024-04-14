292a79560f71fbe64a1ffd50c2960cf176057bd2
QUANTEMEDUTECH <neeraj1511kr@gmail.com>
2024-04-14 23:57:07 +0530

    changes - HF

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,35 +1,49 @@
 import { SiTwitter, SiGithub, SiInstagram } from "react-icons/si";
 import { FiLinkedin } from "react-icons/fi";
 
+import React from "react";
 
-function Footer() {
-  const date = new Date();
-  const year = date.getFullYear();
+const Footer = () => {
   return (
-     <footer className="absolute w-full -bottom-0 bg-gray-800 text-white p-6">
-       <div className="container mx-auto flex flex-wrap items-center justify-between">
-         <div className="w-full md:w-1/2 flex justify-end">
-           <div className="flex space-x-4">
-             <a href="https://twitter.com/yourhandle" target="_blank" rel="noopener noreferrer">
-               <SiTwitter className="text-xl" />
-             </a>
-             <a href="https://github.com/yourhandle" target="_blank" rel="noopener noreferrer">
-               <SiGithub className="text-xl" />
-             </a>
-             <a href="https://www.instagram.com/yourhandle" target="_blank" rel="noopener noreferrer">
-               <SiInstagram className="text-xl" />
-             </a>
-             <a href="https://www.linkedin.com/in/yourhandle" target="_blank" rel="noopener noreferrer">
-               <FiLinkedin className="text-xl" />
-             </a>
-           </div>
-         </div>
-         <div className="w-full mt-4 text-center">
-           <p>© </p>
-         </div>
-       </div>
-     </footer>
+    <section>
+      <div className="flex flex-col items-center justify-center w-full h-60 bg-black gap-4 ">
+        <div className="h-max flex flex-row items-center justify-center gap-8  ">
+          <a
+            href="https://www.youtube.com/@CodeWithSunchitDudeja"
+            target="_blank"
+            className="rounded-full hover:scale-125 bg-white   dark:bg-black hover:border-black hover:border-[2px] transition-all p-2"
+          >
+            <SiTwitter className="text-3xl" />
+          </a>
+          <a
+            href="https://www.youtube.com/@CodeWithSunchitDudeja"
+            target="_blank"
+            className="rounded-full hover:scale-125 bg-white dark:bg-black  hover:border-black hover:border-[2px] transition-all p-2"
+          >
+            <SiGithub className="text-3xl" />
+          </a>
+          <a
+            href="https://www.youtube.com/@CodeWithSunchitDudeja"
+            target="_blank"
+            className="rounded-full hover:scale-125 bg-white  dark:bg-black  hover:border-black hover:border-[2px] transition-all p-2"
+          >
+            <SiInstagram className="text-3xl" />
+          </a>
+          <a
+            href="https://www.youtube.com/@CodeWithSunchitDudeja"
+            target="_blank"
+            className="rounded-full hover:scale-125 bg-white  dark:bg-black  hover:border-black hover:border-[2px] transition-all p-2"
+          >
+            <FiLinkedin className="text-3xl" />
+          </a>
+        </div>
+        <div>
+          <p className=" bg-black text-center text-white text-xs font-normal font-['Source Sans Pro'] ">
+            © 2023 Coding Decoded. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </section>
   );
- }
- 
- export default Footer;
+};
+export default Footer;
--- Components/Header.js
+++ Components/Header.js
@@ -1,25 +1,70 @@
-function Header({ topic, topicName, }) {
+function Header({ topic, topicName }) {
   return (
-     <>
-       {topic && (
-         <div className="pt-24 px-12 mx-auto max-w-7xl">
-           <div className="px-0.5 md:px-7 pt-6 mx-auto flex justify-between items-center">
-             <h3 className="text-2xl font-bold text-gray-700 dark:text-gray-200">{topicName}</h3>
-           </div>
-         </div>
-       )}
- 
-       {!topic && (
-         <div className="pt-24 px-12 mx-auto max-w-7xl">
-           <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
- 
-             {/* Adding the image */}
-             <img src="https://res.cloudinary.com/duveokt0s/image/upload/v1713001600/samples/cloudinary-logo-vector.svg" alt="Reactangle Image" className="w-full h-64 object-cover" />
-           </div>
-         </div>
-       )}
-     </>
+    <>
+      {topic && (
+        <div className=" px-12 mx-auto max-w-7xl mt-[100px]">
+          <div className="px-0.5 md:px-7 pt-6 mx-auto flex justify-between items-center">
+            <h3 className="text-2xl font-bold text-gray-700 dark:text-gray-200">
+              {topicName}
+            </h3>
+          </div>
+        </div>
+      )}
+
+      {!topic && (
+        <div class="p-6">
+          <div class="mx-auto">
+            <div
+              class="relative z-20 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4"
+              style={{
+                backgroundImage:
+                  "linear-gradient(to left bottom, #f87171, #f55979, #ee4085, #e12896, #cb18a9, #bd12b5, #aa16c1, #921fce, #881ed5, #7d1fdc, #6e21e3, #5c24eb)",
+              }}
+            >
+              <div class="pt-10 pb-12 px-6 sm:pt-16 sm:px-8 lg:py-16 lg:pr-0 mt-14">
+                <div class="lg:self-center">
+                  <h2 class="text-3xl leading-9 font-extrabold text-white sm:text-4xl sm:leading-10">
+                    <span class="block">
+                      This is a title for your call to action card
+                    </span>
+                  </h2>
+                  <p class="mt-4 text-base leading-6 text-white">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  <p class="mt-4 text-base leading-6 text-dark-blue-800"></p>
+                  <a
+                    href="#"
+                    target="_blank"
+                    style={{ backgroundColor: "#000" }}
+                    class="mt-8 border border-transparent rounded-md shadow px-6 py-3 inline-flex items-center text-base leading-6 font-medium text-white transition duration-150 ease-in-out"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    target="_blank"
+                    class="ml-4 mt-8 text-white font-bold text-sm underline"
+                  >
+                    Learn more
+                  </a>
+                </div>
+              </div>
+              <div class="relative pb-3/5 -mt-6 md:pb-1/2">
+                <img
+                  class="absolute inset-0 w-full h-full transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-10 lg:translate-y-20"
+                  src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw5fHxsYXB0b3B8ZW58MHwwfHx8MTY5NzQ0ODQ4M3ww&ixlib=rb-4.0.3&q=80&w=1080"
+                  alt="Background"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
- }
- 
- export default Header;
+}
+
+export default Header;
